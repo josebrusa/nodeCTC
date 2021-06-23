ed4d7048e443073a6fed336657dc13fd1183cd51
jose Brusa <josebrusa@outlook.com>
2021-06-23 20:41:22 -0300

    promiseHell

---
+++ 02-fundamentos/promise.js
@@ -0,0 +1,69 @@
+const empleados = [
+    {
+        id: 1,
+        nombre: 'Jose'
+    },
+    {
+        id: 2,
+        nombre: 'Fernando'
+    },
+    {
+        id: 3,
+        nombre: 'Pablo'
+    }
+];
+
+const salarios = [
+    {
+        id: 1,
+        salario: 1000
+    },
+    {
+        id: 2,
+        salario: 2000
+    },
+    {
+        id: 3
+    }
+];
+
+
+const getEmpleado = (id) => {
+    const empleado = empleados.find(e => e.id === id)?.nombre;
+
+    return new Promise(( resolve, reject ) => {
+        ( empleado )
+        ? resolve( empleado )
+        : reject(`No existe empleado con el id: ${ id }`)
+        }
+    );
+}
+
+const getSalario = (id) => {
+    const salario = salarios.find(s => s.id === id)?.salario;
+
+    return new Promise(( resolve, reject ) => {
+        ( salario )
+        ? resolve( salario )
+        : reject(`No existe salario del id: ${id}`)
+    });
+}
+
+const id = 3;
+
+// getEmpleado(id)
+//     .then(empleado => console.log(empleado))
+//     .catch(err => console.log(err))
+// getSalario(id)
+//     .then(salario => console.log(salario))
+//     .catch(err => console.log(err))
+
+getEmpleado(id)
+    .then(empleado =>{
+        getSalario(id)
+            .then(salario => {
+                console.log('El empleado:', empleado, 'tiene un salario:', salario);
+            })
+            .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
