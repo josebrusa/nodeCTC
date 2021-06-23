d16f35db76a16e82242ede0921c453d785e2551f
jose Brusa <josebrusa@outlook.com>
2021-06-23 16:43:20 -0300

    template literals

---
+++ 02-fundamentos/template-string.js
@@ -0,0 +1,16 @@
+const nombre = 'dead pool';
+const nombreReal = 'wade winston';
+
+const normal = nombre +' is '+ nombreReal;
+const template = `El nombre de superoHero es ${nombre} y su verdadera identidad es ${nombreReal}`
+
+console.log(normal);
+console.log(template);
+
+
+const html = `
+<h1>Hola</h1>
+<p>Mundo</p>
+`;
+
+console.log(html);
