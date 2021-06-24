8c510f92ecc9c7075ba0d0e0e1a887264c41cf7d
jose Brusa <josebrusa@outlook.com>
2021-06-23 23:09:33 -0300

    agregando fs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,9 +1,21 @@
-console.clear();
-console.log('===================')
-console.log('    Tabla del 5    ');
-console.log('===================')
+const fs = require('fs')
 
-const base = 5;
+console.clear();
+const base = 3;
+
+console.log('======================')
+console.log(`     Tabla del ${base}`);
+console.log('======================')
+
+let salida = '';
+
 for (let i = 1; i <= 10; i++) {
-    console.log(`${base} x ${i} = ${base*i}`);
+    salida += `${base} x ${i} = ${base*i}\n`;
 }
+
+console.log(salida);
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+    if(err) throw err;
+    console.log(`tabla-${base}.txt creado`);
+})
