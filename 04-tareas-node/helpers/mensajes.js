311c00fd885d78df883d0565089eb55b685f3571
jose Brusa <josebrusa@outlook.com>
2021-06-28 17:34:06 -0300

    init proyect task, stdin-stdout-readline

---
+++ 04-tareas-node/helpers/mensajes.js
@@ -0,0 +1,45 @@
+require('colors');
+
+
+const mostrarMenu = () => {
+    console.clear();
+    console.log('========================='.white.bgGreen);
+    console.log('* Seleccione una opcion *'.green);
+    console.log('=========================\n'.white.bgGreen);
+
+    console.log(`${'1.'.cyan} Crear tarea`);
+    console.log(`${'2.'.cyan} Listar tarea`);
+    console.log(`${'3.'.cyan} Listar tareas completadas`);
+    console.log(`${'4.'.cyan} Listar tareas pendientes`);
+    console.log(`${'5.'.cyan} Completar tare(s)`);
+    console.log(`${'6.'.cyan} Borrar tarea`);
+    console.log(`${'0.'.cyan} Salir \n`);
+
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    readline.question('Seleccion una opcion: ', (opt) => {
+        readline.close();
+    })
+
+}
+
+
+const pausa = () => {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    readline.question(`\nPresciona ${'ENTER'.cyan} para continuar`, (opt) => {
+        readline.close();
+    });
+}
+
+
+module.exports = {
+    mostrarMenu,
+    pausa
+}
