80463b1855751aeb852fc0db7c0ec75868059051
jose Brusa <josebrusa@outlook.com>
2021-06-28 20:00:32 -0300

    repetir el menu de forma infinita

--- 04-tareas-node/app.js
+++ 04-tareas-node/app.js
@@ -9,8 +9,12 @@ console.clear()
 const main = async () => {
     console.log('hola mundo');
 
-    mostrarMenu();
-    pausa();
+    let opt = '';
+    do {
+        opt = await mostrarMenu();
+        console.log({ opt });
+        if(opt !== '0') await pausa();
+    } while ( opt !== '0' );
 }
 
 main();
