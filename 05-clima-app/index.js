5a705170515e599240cde8dbb1ef59a4c2900af5
jose Brusa <josebrusa@outlook.com>
2021-07-14 00:42:34 -0300

    add menu

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -1,13 +1,34 @@
 
-const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer')
+const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
 
 const main = async() => {
+
+    const busqeudas = new Busquedas();
     let opt;
 
         do {
 
             opt = await inquirerMenu();
-            console.log({ opt });
+
+            switch( opt ) {
+                case 1:
+
+                    const lugar = await leerInput('Ciudad: ');
+                    console.log( lugar );
+
+
+
+
+                    console.log('\nInformacion de la ciudades\n'.cyan);
+                    console.log('Ciudad: ');
+                    console.log('Latitud: ');
+                    console.log('Long: ');
+                    console.log('Temperatura: ');
+                    console.log('Minima: ');
+                    console.log('Maxima: ');
+                break;
+            }
 
                 if( opt !== 0 ) await pausa();
 
---
+++ 05-clima-app/models/busquedas.js
@@ -0,0 +1,20 @@
+
+
+
+class Busquedas {
+
+    historial = [''];
+
+    constructor(){
+
+    }
+
+    async ciudad(  lugar = '' ) {
+        console.log( lugar );
+
+        return [];
+    }
+}
+
+
+module.exports = Busquedas;
