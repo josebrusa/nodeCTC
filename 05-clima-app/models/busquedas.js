e1d7e741c124434d1791f9c797dcd41803f39396
jose Brusa <josebrusa@outlook.com>
2021-07-14 19:00:35 -0300

    mapbox api

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -13,7 +13,7 @@ class Busquedas {
     async ciudad(  lugar = '' ) {
 
         try {
-            const resp = await axios.get( 'https://reqres.in/api/users?page=2' );
+            const resp = await axios.get( 'https://api.mapbox.com/geocoding/v5/mapbox.places/Cor.json?access_token=pk.eyJ1Ijoiam9zZWJydXNhIiwiYSI6ImNrcjQwa2w1dDJyNmMycHJ1aDVqbDdyZXYifQ.CjerDcFVaVbYX4qg-lL2sQ&limit=5&language=es' );
             console.log( resp.data );
 
             return [];
