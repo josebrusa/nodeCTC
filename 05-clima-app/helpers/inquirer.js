75de9e63a6b142dd4e1ce1a03877eade01643df1
jose Brusa <josebrusa@outlook.com>
2021-07-14 00:21:49 -0300

    add menu

--- 05-clima-app/helpers/inquirer.js
+++ 05-clima-app/helpers/inquirer.js
@@ -1,7 +1,53 @@
 const inquirer = require('inquirer');
 require('colors');
 
+const preguntas = [
+    {
+        type:'list',
+        name:'opcion',
+        message:'¿Que deseas hacer?',
+        choices:[
+            {
+                value: 1,
+                name:`${ '1.'.cyan } Buscar ciudad`
+            },
+            {
+                value: 2,
+                name:`${ '2.'.cyan } Historial`
+            },
+            {
+                value: 3,
+                name:`${ '3.'.cyan } Salir`
+            }
+        ]
+    }
+];
 
+
+const inquirerMenu = async () => {
+    console.clear();
+
+    console.log('============================'.bgGreen);
+    console.log(' * Selecciones una opcion *'.green);
+    console.log('============================\n'.bgGreen);
+
+    const { opcion } = await inquirer.prompt( preguntas );
+    return opcion;
+
+}
+
+
+const pausa = async () => {
+    const question = [
+        {
+            type:'input',
+            name:'enter',
+            message:`Presiona ${'enter'.cyan} para continuar`
+        }
+    ];
+    console.log('\n');
+    await inquirer.prompt( question );
+}
 
 const leerInput = async( message ) => {
     const question = [
@@ -24,5 +70,7 @@ const leerInput = async( message ) => {
 
 
 module.exports = {
-    leerInput
+    leerInput,
+    inquirerMenu,
+    pausa
 }
