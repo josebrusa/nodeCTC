c63c9e5815f766e38cc6f544cdd16daa926fe731
jose Brusa <josebrusa@outlook.com>
2021-07-14 20:33:37 -0300

    creando instancia en axios

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -1,27 +1,46 @@
-const axios = require("axios");
+const axios = require('axios');
 
 
 
 class Busquedas {
 
-    historial = [''];
+    historial = ['Cordoba'];
 
     constructor(){
 
     }
 
+    get paramsMapbox(){
+        return {
+            'access_token': 'pk.eyJ1Ijoiam9zZWJydXNhIiwiYSI6ImNrcjQwa2w1dDJyNmMycHJ1aDVqbDdyZXYifQ.CjerDcFVaVbYX4qg-lL2sQ',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
     async ciudad(  lugar = '' ) {
 
         try {
-            const resp = await axios.get( 'https://api.mapbox.com/geocoding/v5/mapbox.places/Cor.json?access_token=pk.eyJ1Ijoiam9zZWJydXNhIiwiYSI6ImNrcjQwa2w1dDJyNmMycHJ1aDVqbDdyZXYifQ.CjerDcFVaVbYX4qg-lL2sQ&limit=5&language=es' );
+
+            const intance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
+                params: this.paramsMapbox
+            });
+            const resp = await intance.get();
             console.log( resp.data );
 
-            return [];
+            return []
+
         } catch (error) {
-            return [];
+            return []
         }
     }
 }
 
 
+
 module.exports = Busquedas;
+
+
+
+// https://api.mapbox.com/geocoding/v5/mapbox.places/Cor.json?access_token=pk.eyJ1Ijoiam9zZWJydXNhIiwiYSI6ImNrcjQwa2w1dDJyNmMycHJ1aDVqbDdyZXYifQ.CjerDcFVaVbYX4qg-lL2sQ&limit=5&language=es
