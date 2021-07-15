878acc4416a7baf3f4751c829b2745d147c72651
jose Brusa <josebrusa@outlook.com>
2021-07-14 23:40:46 -0300

    listar lugares

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -16,16 +16,20 @@ const main = async() => {
                 switch( opt ) {
 
                     case 1:
-
+                        // Mostrar mensaje
                         const termino = await leerInput( 'Ciudad: ' );
+                        // Buscar  los lugares
                         const lugares = await busquedas.ciudad( termino );
+                        // Seleccionar lugar
                         const id = await listarLugares( lugares );
-                        console.log( { id } );
+                        const lugarSelect = lugares.find( l => l.id === id );
+
+                        // Clima
 
                         console.log('\nInformacion de la ciudad\n'.cyan);
-                        console.log('Ciudad: ');
-                        console.log('Latitud: ');
-                        console.log('Long: ');
+                        console.log('Ciudad: ', lugarSelect.nombre );
+                        console.log('Lat: ', lugarSelect.lat );
+                        console.log('Lng: ', lugarSelect.lng );
                         console.log('Temperatura: ');
                         console.log('Minima: ');
                         console.log('Maxima: ');
