e30be9d6633660ffbfa8d4a4db4badc33678a886
jose Brusa <josebrusa@outlook.com>
2021-07-20 23:36:20 -0300

    leer archivo y capitalizar

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -46,7 +46,7 @@ const main = async() => {
 
                     case 2:
 
-                        busquedas.historial.forEach(( lugar, i ) => {
+                        busquedas.historialCapitalizado.forEach(( lugar, i ) => {
                             const idx = `${i + 1}.`.cyan;
                             console.log(`${ idx } ${ lugar }`);
                         })
--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -9,7 +9,16 @@ class Busquedas {
     dbPath = './db/database.json';
 
     constructor(){
+        this.leerDB();
+    }
 
+    get historialCapitalizado(){
+        return this.historial.map( lugar => {
+            let palabras = lugar.split(' ');
+            palabras = palabras.map( p => p[0].toUpperCase() + p.substring(1));
+
+            return palabras.join(' ');
+        });
     }
 
     get paramsMapbox(){
@@ -27,6 +36,7 @@ class Busquedas {
             lang: 'es'
         }
     }
+
 
     async ciudad(  lugar = '' ) {
 
@@ -78,6 +88,7 @@ class Busquedas {
         if(this.historial.includes( lugar.toLocaleLowerCase() )){
             return;
         }
+        this.historial = this.historial.splice(0,5);
         this.historial.unshift( lugar.toLocaleLowerCase() );
 
         // Grabar DB
@@ -92,7 +103,14 @@ class Busquedas {
     };
 
     leerDB(){
+        if( !fs.existsSync( this.dbPath )){
+            return;
+        }
 
+        const info = fs.readFileSync( this.dbPath, { encoding: 'utf8' });
+        const data = JSON.parse(info);
+
+        this.historial = data.historial;
     };
 }
 
